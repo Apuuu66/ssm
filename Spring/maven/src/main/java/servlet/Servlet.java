3ae3485b4e029d1605c9449e5ed880009c4c3d5f
xiaokeai <466007639@qq.com>
2018-01-02 00:27:27 +0800

    'maven_parent_child'

---
+++ Spring/maven/src/main/java/servlet/Servlet.java
@@ -0,0 +1,14 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Fionar
+ * Time: 2017/12/29 0:23
+ */
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
